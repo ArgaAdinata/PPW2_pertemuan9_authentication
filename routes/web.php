fa70e5dc24fee3c1ede7bd92d8b12b36b4384d79
Arga Adinata <argaadinata10@gmail.com>
2024-10-23 19:49:36 +0700

    first commit

---
+++ routes/web.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+use App\Http\Controllers\BukuController;
+use App\Http\Controllers\Auth\LoginRegisterController;
+
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/buku', [BukuController::class, 'index'])->name('buku.index');
+Route::get('/buku/search',[BukuController::class, 'search'])->name('buku.search');
+
+Route::middleware(['auth'])->group(function () {
+    Route::get('/buku/create', [BukuController::class, 'create'])->name('buku.create');
+    Route::post('/buku', [BukuController::class, 'store'])->name('buku.store');
+    Route::delete('/buku/{id}', [BukuController::class, 'destroy'])->name('buku.destroy');
+    Route::get('/buku/{id}/edit', [BukuController::class, 'edit'])->name('buku.edit');
+    Route::post('/buku/{id}', [BukuController::class, 'update'])->name('buku.update');
+});
+
+Route::controller(LoginRegisterController::class)->group(function() {
+    Route::get('/register', 'register')->name('register');
+    Route::post('/store', 'store')->name('store');
+    Route::get('/login', 'login')->name('login');
+    Route::post('/authenticate', 'authenticate')->name('authenticate');
+    Route::get('/dashboard', 'dashboard')->name('dashboard');
+    Route::post('/logout', 'logout')->name('logout');
+   });
